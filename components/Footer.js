7b13097be6c3a9f1b0016ca1267e1c05cce51a62
Pujaksh-Design-Hub-Coaching <166235113+Pujaksh-Design-Hub-Coaching@users.noreply.github.com>
2024-08-24 21:38:40 +0530

    updates

--- components/Footer.js
+++ components/Footer.js
@@ -65,7 +65,7 @@ const Footer = () => {
 
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-          <img className="ml-3 text-xl h-16" src="/futurescape logo.png" />
+          <img className="ml-3 text-xl h-16" src="/FuturescapeLogo.png" />
         </a>
         <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-700 sm:py-2 sm:mt-0 mt-4">
           © 2024 FutureScape
